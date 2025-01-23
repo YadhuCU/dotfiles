033ed88ee0f8c6506dcb44587293f9d77babca3e
Yadhukrishna CU <yadhukrishna.unnikrishnan@memphis-marine.com>
2025-01-23 17:16:50 +0530

    feat: add error diagonise and new keymap for adding space above and below

--- .config/nvim/lua/theprimeagen/remap.lua
+++ .config/nvim/lua/theprimeagen/remap.lua
@@ -47,6 +47,10 @@ vim.keymap.set("n", "[a", ":cprev<CR>")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+-- add Empty line
+vim.keymap.set("n", "[<Space>", "mzO<C-c>`z")
+vim.keymap.set("n", "]<Space>", "mzo<C-c>`z")
+
 --[[
 -- TODO: change by requirement;
 vim.keymap.set(
--- .config/nvim/lua/theprimeagen/set.lua
+++ .config/nvim/lua/theprimeagen/set.lua
@@ -1,3 +1,19 @@
+function get_diagnostic_counts()
+    local diagnostics = vim.diagnostic.get(0)
+    local error_count = 0
+    local warning_count = 0
+
+    for _, diagnostic in ipairs(diagnostics) do
+        if diagnostic.severity == vim.diagnostic.severity.ERROR then
+            error_count = error_count + 1
+        elseif diagnostic.severity == vim.diagnostic.severity.WARN then
+            warning_count = warning_count + 1
+        end
+    end
+
+    return string.format("E:%d W:%d", error_count, warning_count)
+end
+
 vim.opt.guicursor = ""
 
 vim.opt.nu = true
@@ -31,6 +47,15 @@ vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 
 vim.opt.clipboard = "unnamedplus"
+
+vim.o.statusline = "%f"       -- File name
+vim.o.statusline = vim.o.statusline .. "%m"  -- Modified flag
+vim.o.statusline = vim.o.statusline .. "%r"  -- Readonly flag
+vim.o.statusline = vim.o.statusline .. "%h"  -- Help flag
+vim.o.statusline = vim.o.statusline .. "%w"  -- Preview window flag
+vim.o.statusline = vim.o.statusline .. " %{v:lua.get_diagnostic_counts()}" -- Diagnostic counts
+vim.o.statusline = vim.o.statusline .. "%=%l,%c" -- Line and column number
+
 
 --[[
 
